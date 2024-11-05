81b1ffa4d685af0e52128760563a5e849aea4a45
Pushpraj Dwivedi <pushprajdwivedi001@gmail.com>
2024-11-05 18:25:27 +0530

    formated

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,24 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    "use strict";
+  "use strict";
 
-    // FAQ ACCORDION JS START =====
-    const accordionContainers = document.querySelectorAll(".sa-accordion");
+  // FAQ ACCORDION JS START =====
+  const accordionContainers = document.querySelectorAll(".sa-accordion");
 
-    accordionContainers.forEach(accordionContainer => {
-        const accordionItems = accordionContainer.querySelectorAll(".sa-single-accordion-item");
-        accordionItems.forEach((accordionItem) => {
-            accordionItem.onclick = () => {
-                const openedItems = accordionContainer.querySelector(".sa-single-accordion-item.open");
-                if (accordionItem.classList.contains("open")) {
-                    openedItems.classList.remove("open")
-                } else {
-                    accordionItem.classList.toggle("open");
-                    if (openedItems) {
-                        openedItems.classList.remove("open")
-                    }
-                }
-            }
-        })
+  accordionContainers.forEach((accordionContainer) => {
+    const accordionItems = accordionContainer.querySelectorAll(
+      ".sa-single-accordion-item",
+    );
+    accordionItems.forEach((accordionItem) => {
+      accordionItem.onclick = () => {
+        const openedItems = accordionContainer.querySelector(
+          ".sa-single-accordion-item.open",
+        );
+        if (accordionItem.classList.contains("open")) {
+          openedItems.classList.remove("open");
+        } else {
+          accordionItem.classList.toggle("open");
+          if (openedItems) {
+            openedItems.classList.remove("open");
+          }
+        }
+      };
     });
-    //===== FAQ ACCORDION JS END
+  });
+  //===== FAQ ACCORDION JS END
 });
